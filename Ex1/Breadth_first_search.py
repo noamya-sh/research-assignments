49d51e5b40b01c37bece84ebd4ac914bfa7740a3
noamya-sh <noamyashany@gmail.com>
2022-10-30 18:20:01 +0200

    test

--- Ex1/Breadth_first_search.py
+++ Ex1/Breadth_first_search.py
@@ -11,8 +11,23 @@ def Breadth_first_search(start: tuple, end: tuple, neighbor_function: Callable) 
         neighbor_list = neighbor_function(node)
         for neighbor in neighbor_list:
             if neighbor not in visited:
-                queue.append((neighbor,path_node + [neighbor]))
-                visited[neighbor] = True
                 if neighbor == end:
                     return path_node + [neighbor]
+                queue.append((neighbor, path_node + [neighbor]))
+                visited[neighbor] = True
+
     return []
+
+
+g = []
+for i in range(10):
+    for j in range(10):
+        g.append((i, j))
+
+
+def n(node: tuple):
+    x, y = node
+    return [(x, y - 1), (x + 1, y), (x - 1, y), (x, y + 1)]
+
+
+print(Breadth_first_search((0, 0), (3, 1), n))

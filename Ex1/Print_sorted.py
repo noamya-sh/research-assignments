c3f0b4f979e6d588db0cd4d3212df466288189a1
noamya-sh <noamyashany@gmail.com>
2022-10-31 15:52:37 +0200

    add doctest

--- Ex1/Print_sorted.py
+++ Ex1/Print_sorted.py
@@ -1,9 +1,56 @@
-def print_sorted(x):
-    list =[]
-    for i in enumerate(x):
-        list.append(str(i))
-    list.sort()
-    print(list)
+# import json
+#
+#
+# def print_sorted(x):
+#     list = []
+#     for i in enumerate(x):
+#         list.append(str(i))
+#     list.sort()
+#     print(list)
+#
+def helpi(struct):
+    input_class = type(struct)
+    if input_class == dict:
+        output_dict = {}
+        for k, v in struct.items():
+            if type(k) in [list, dict, tuple, set]:
+                if type(v) in [list, dict, tuple, set]:
+                    output_dict[helpi(k)] = helpi(v)
+                else:
+                    output_dict[helpi(k)] = str(v)
+            else:
+                if type(v) in [list, dict, tuple, set]:
+                    output_dict[str(k)] = helpi(v)
+                else:
+                    output_dict[str(k)] = str(v)
+        return str(sorted(output_dict))
+    else:
+        output_list = []
+        for i in struct:
+            if type(i) in [list, dict, tuple, set]:
+                output_list.append(helpi(i))
+            else:
+                output_list.append(str(i))
+        return str(sorted(input_class(output_list)))
 
 
-print_sorted([4, {'a': "v", 'c': [2]}, (1, 3,2, 3)])
+#
+# def f(struct):
+#     for i in struct:
+#         if type(i) in [list, dict, tuple, set]:
+#             return f(i)
+#     return str(struct)
+#
+# # print_sorted([4, {'a': "v", 'c': [2]}, (1, 3,2, 3)])
+r = [4, [9, 7], {'d': "v", 'c': [2]}, (1, 3, 2, 3), 5]
+print(helpi(r))
+# k = []
+# for i in r:
+#     if type(i) in [list, dict, tuple, set]:
+#         k.append(str(sorted(i)))
+#     else:
+#         k.append(str(i))
+# k.sort()
+# print(k)
+# # helpi(r)
+# # print(sorted(r))
--- Ex1/Safe_call.py
+++ Ex1/Safe_call.py
@@ -1,10 +1,10 @@
-
+import doctest
 from typing import Callable
 
-"""Return the factorial of n, an exact integer >= 0.
-    >>> [factorial(n) for n in range(6)]
-    [1, 1, 2, 6, 24, 120]
-    >>> factorial(30)
+"""Checks if the arguments match the annotations of the given function..
+    >>> def f(y: float, x: int, z=5):
+            return x + y + z
+    >>> as
     265252859812191058636308480000000
     >>> factorial(-1)
     Traceback (most recent call last):
@@ -24,15 +24,47 @@ from typing import Callable
     OverflowError: n too large
     """
 
-def safe_call(func: Callable ,*args ,**kwargs):
+
+def safe_call(func: Callable, *args, **kwargs):
+    """Checks if the arguments match the annotations of the given function.
+        >>> def f(y: float, x: int, z=5): return x + y + z
+        >>> safe_call(f,2,1,7)
+        Traceback (most recent call last):
+            ...
+        ValueError: Annotation does not match
+        >>> safe_call(f,"fff",3,6)
+        Traceback (most recent call last):
+            ...
+        ValueError: Annotation does not match
+        >>> safe_call(f,2)
+        Traceback (most recent call last):
+            ...
+        ValueError: Arguments are missing
+        >>> safe_call(f,2.5,1,7)
+        >>> def k(a: list, b: tuple, c:float): return 2
+        >>> safe_call(k,[2,3],(1,9,0),7.2)
+        >>> safe_call(k,(1,"j",0),[1,15],2.1)
+        Traceback (most recent call last):
+            ...
+        ValueError: Annotation does not match
+        >>> safe_call(k,2)
+        Traceback (most recent call last):
+            ...
+        ValueError: Arguments are missing
+        """
+
+    # get annotations of given function
     annotations = func.__annotations__
+    # get arguments types
     if args:
         l = locals()['args']
-        vars =dict(zip(list(annotations.keys()), l))
-        print("args", vars)
+        vars = dict(zip(list(annotations.keys()), l))
     else:
         vars = locals()['kwargs']
-    print("vars", vars)
+    # throw exception if arguments are missing
+    if len(vars) < len(annotations):
+        raise ValueError("Arguments are missing")
+
     for x in annotations.keys():
         if annotations[x] is not type(vars[x]):
             raise ValueError("Annotation does not match")

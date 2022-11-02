f108684087e82feda869665fb7537be3d072278f
noamya-sh <noamyashany@gmail.com>
2022-11-02 09:30:23 +0200

    final

--- Ex1/Safe_call.py
+++ Ex1/Safe_call.py
@@ -1,30 +1,6 @@
 import doctest
 from typing import Callable
 
-"""Checks if the arguments match the annotations of the given function..
-    >>> def f(y: float, x: int, z=5):
-            return x + y + z
-    >>> as
-    265252859812191058636308480000000
-    >>> factorial(-1)
-    Traceback (most recent call last):
-        ...
-    ValueError: n must be >= 0
-    Factorials of floats are OK, but the float must be an exact integer:
-    >>> factorial(30.1)
-    Traceback (most recent call last):
-        ...
-    ValueError: n must be exact integer
-    >>> factorial(30.0)
-    265252859812191058636308480000000
-    It must also not be ridiculously large:
-    >>> factorial(1e100)
-    Traceback (most recent call last):
-        ...
-    OverflowError: n too large
-    """
-
-
 def safe_call(func: Callable, *args, **kwargs):
     """Checks if the arguments match the annotations of the given function.
         >>> def f(y: float, x: int, z=5): return x + y + z

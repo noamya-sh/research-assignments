8aa99653d01705c29d1cee8bc5ec41f52bbd4ecd
noamya-sh <noamyashany@gmail.com>
2022-11-09 17:23:55 +0200

    list3D

--- Ex2/MultiArray.py
+++ Ex2/MultiArray.py
@@ -1,17 +1,22 @@
 class MultiArray(list):
 
     def __init__(self, *args):
-        super().__init__()
-        self.l = args
+        list.__init__(self, *args)
 
     def __getitem__(self, item):
-        if len(item) == 1:
-            return self.l[item]
+        if len(str(item)) == 1:
+            return super(MultiArray, self).__getitem__(item)
         try:
-            return self.l[item[0]][item[1]][item[2]]
+            if len(item) == 2:
+                return super(MultiArray, self).__getitem__(item[0])[item[1]]
+            if len(item) == 3:
+                return super(MultiArray, self).__getitem__(item[0])[item[1]][item[2]]
         except:
             print("err")
 
 
-x = MultiArray([[1, 2, 3], [1, 2, 5]])
-print(x[0,1,2])
+x = MultiArray([[[91, 42, 56], [19, 14, 15]], [[1, 2, 3], [9, 4, 2]], [[11, 47, 53], [85, 21, 35]]])
+print(x[1])
+print(x[1, 1])
+print(x[1, 1, 2])
+print(x.pop(2))

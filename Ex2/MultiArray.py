c45331e911a07c28b828e7738457f0bfe97ad450
noamya-sh <noamyashany@gmail.com>
2022-11-10 13:22:22 +0200

    fix to multiarray

--- Ex2/MultiArray.py
+++ Ex2/MultiArray.py
@@ -1,3 +1,10 @@
+def help(l, ind):
+    if len(ind) == 1:
+        return l[ind[0]]
+    else:
+        return help(l[ind[0]], ind[1:])
+
+
 class MultiArray(list):
 
     def __init__(self, *args):
@@ -6,13 +13,15 @@ class MultiArray(list):
     def __getitem__(self, item):
         if len(str(item)) == 1:
             return super(MultiArray, self).__getitem__(item)
-        try:
-            if len(item) == 2:
-                return super(MultiArray, self).__getitem__(item[0])[item[1]]
-            if len(item) == 3:
-                return super(MultiArray, self).__getitem__(item[0])[item[1]][item[2]]
-        except:
-            print("err")
+        else:
+            item = list(item)
+            return help(super(MultiArray, self).__getitem__(item[0]), item[1:])
+            # if len(item) == 2:
+            #     return super(MultiArray, self).__getitem__(item[0])[item[1]]
+            # if len(item) == 3:
+            #     return super(MultiArray, self).__getitem__(item[0])[item[1]][item[2]]
+        # except:
+        #     print("err")
 
 
 x = MultiArray([[[91, 42, 56], [19, 14, 15]], [[1, 2, 3], [9, 4, 2]], [[11, 47, 53], [85, 21, 35]]])

9a77ff15ebb2015ab5fb4540b1ff5a9612518c37
noamya-sh <noamyashany@gmail.com>
2022-11-15 12:54:01 +0200

    doctest print_mail

--- Ex3/Print_mail.py
+++ Ex3/Print_mail.py
@@ -1,8 +1,20 @@
+import doctest
 import re
 
 
 def print_mail(path):
-    reg = re.compile(r'^\w+([._-]\w+)?@(\w+[.-])+\w{2,}$')
+    """
+    >>> print_mail("prefix_mail_test.txt")
+    valid email:  ['a@gmail.com', 'a-a@gmail.com', 'adf34@gmail.com', 'a_a@gmail.com', 'gt5-56@gmail.com', 'Ahron@gmail.com', 'r.r@gmail.com', 'rtret_56@gmail.com']
+    invalid email:  ['ad%f34@gmail.com', 'levi%@gmail.com', 'ghg-@gmail.com', 'levi%@gmail.com', 'fg..tr@gmail.com', 'fg_.tr@gmail.com']
+    >>> print_mail("domain_mail_test.txt")
+    valid email:  ['aryeh@gmail76.com', 'aryeh@re.re.re.re', 'aryeh@tr-tr.com', 'aryeh@gmail.com.il']
+    invalid email:  ['aryeh@re', 'aryeh@re.87', 'aryeh@gmail..com', 'aryeh@-tr.com', 'aryeh@gma%il.com', 'aryeh@gmail.c']
+    >>> print_mail("official_test.txt")
+    valid email:  ['abc-d@mail.com', 'abc.def@mail.com', 'abc@mail.com', 'abc_def@mail.com', 'abc.def@mail.cc', 'abc.def@mail-archive.com', 'abc.def@mail.org', 'abc.def@mail.com']
+    invalid email:  ['abc-@mail.com', 'abc..def@mail.com', '.abc@mail.com', 'abc#def@mail.com', 'abc.def@mail.c', 'abc.def@mail#archive.com', 'abc.def@mail', 'abc.def@mail..com']
+    """
+    reg = re.compile(r'^[A-Za-z0-9]+([._-]?[A-Za-z0-9]+)*@([A-Za-z0-9]+[.-])+[A-Za-z]{2,}$')
     with open(path, "r") as file:
         splited = re.split(r'[\n\t\r ]+', file.read())
         valid = []
@@ -17,4 +29,5 @@ def print_mail(path):
         print("invalid email: ", inValid)
 
 
-print_mail("a.txt")
+if __name__ == '__main__':
+     doctest.testmod()

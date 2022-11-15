add48f0e84f14b133e5cb23ac30a02e3543d50f8
noamya-sh <noamyashany@gmail.com>
2022-11-15 12:12:11 +0200

    fix print_mail

--- Ex3/Print_mail.py
+++ Ex3/Print_mail.py
@@ -1,15 +1,20 @@
 import re
 
-# regex1 = "^[A-Za-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
-regex1 = "^[A-Za-z0-9]+[\._]?\w+[@]\w+[.]\w{2,}$"
-reg = re.compile(regex1)
-with open("a.txt", "r") as a:
-    b = a.read()
-    c = re.split(r'[\n ]+', b)
-    for d in c:
-        obj = reg.search(d)
-        # obj = re.search(r'[\w.]+\@[\w.]+', d)
-        if obj:
-            print(d, "Valid Email")
-        else:
-            print(d, "Invalid Email")
+
+def print_mail(path):
+    reg = re.compile(r'^\w+([._-]\w+)?@(\w+[.-])+\w{2,}$')
+    with open(path, "r") as file:
+        splited = re.split(r'[\n\t\r ]+', file.read())
+        valid = []
+        inValid = []
+        for adress in splited:
+            mail = reg.search(adress)
+            if mail:
+                valid.append(adress)
+            else:
+                inValid.append(adress)
+        print("valid email: ", valid)
+        print("invalid email: ", inValid)
+
+
+print_mail("a.txt")
